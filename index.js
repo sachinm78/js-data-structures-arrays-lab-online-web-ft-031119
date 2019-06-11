341a4596bbfb5016dcb65e10e6487a5cf73ed9fa
Sachin Mahajan <sachinm78@gmail.com>
2019-06-11 21:19:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,10 @@ function destructivelyPrependDriver (name) {
   drivers.unshift(name);
 }
 
+function destructivelyRemoveLastDriver () {
+  drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver () {
+  drivers.shift();
+}
